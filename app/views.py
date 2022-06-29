1410f7f3aaf1374b5dea270d7095265d2fd2bbda
asadfan <asadahmedfan1.com>
2022-06-29 18:58:39 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+def mdboot4(request):
+    return render(request,'mdboot4.html')
+def mdboot5(request):
+    return render(request,'mdboot5.html')
+def bootcdn(request):
+    return render(request,'bootcdn.html')
